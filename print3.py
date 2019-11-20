b04857f48a3112625f9e9362b49fe15087789183
Moksh Dhama <47278462+mokshdhama27@users.noreply.github.com>
2019-11-20 11:16:57 +0530

    Create print3.py

---
+++ print3.py
@@ -0,0 +1,29 @@
+#This program is to understand assigning of variables variables
+
+
+my_name = 'Moksh Dhama'
+my_age = 20 
+my_height = 75 #inches
+my_weight = 60 
+my_eyes = 'dark brown'
+my_hair = 'black'
+
+# converting kilogram into pounds
+# 1 kg = 2.20 pound
+# 5 kg = 11.02 pound
+# 1 pound = 0.45359237 kg
+# 5 pound = 2.26796 kg
+
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} inches tall.")
+print(f"He's {my_weight} pounds heavy.")
+print(f"Actually he don't look like that heavy")
+print(f"He's got {my_eyes} eyes and {my_hair} hair")
+
+#going to perforn mathematical function
+
+# Get exactly right total
+
+my_total =  my_age + my_height + my_weight
+print(f"If i add my age, height and weight i get {my_total}.")
